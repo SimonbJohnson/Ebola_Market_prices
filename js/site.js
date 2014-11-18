8ddc87591f2c61ad938af22a3af78597439eebaa
SimonbJohnson <simonbjohnson@gmail.com>
2014-11-18 17:30:54 +0000

    Add mouse over functionality, started map

--- js/site.js
+++ js/site.js
@@ -11,6 +11,9 @@ var byDate = cf.dimension(function(d){return d.date;});
 var groupByDateCount = byDate.group();
 var groupByDateSum = byDate.group().reduceSum(function(d) {return d.mp_price;});
 
+var currentProduct;
+var currentCountry;
+
 function filterChartData(country,product){
     byCountry.filterAll();
     byProduct.filterAll();
@@ -32,26 +35,41 @@ function filterChartData(country,product){
     return data;
 }
 
+function filterBarData(bar){
+    byCountry.filterAll();
+    byProduct.filterAll();
+    byDate.filterAll();
+    byCountry.filterExact(currentCountry);
+    byProduct.filterExact(currentProduct);    
+    byDate.filterExact(bar);
+    return byDate.top(Infinity);
+};
+
 function onCountrySelect(country){
     var html ="";
+    var i = 0;
     $('#spark_charts').html(html);
+    transitionCountryMap(country);
     products.forEach(function(e){
         if(e.country==country){
             e.products.forEach(function(e){
                 $('#spark_charts').append('<p>'+e+'</p><div id="'+e.replace(/\s+/g, '')+'" class="sparkchart"></div>');
                 generateSparkBars(e,filterChartData(country,e));
+                if(i==0){
+                    currentCountry = country;
+                    currentProduct = e;
+                    transitionBarChart(filterChartData(country,e));
+                    i++;
+                }
             });
         }
     });
-   
 }
 
 function generateSparkBars(product,data){
-    //var height=$(id).height();
-    //var width=$(id).width();;
     var id='#'+product.replace(/\s+/g, '');
-    var height=50;
-    var width=200;
+    var height=$(id).height();
+    var width=$(id).width();
     var avg=0;
     var total=0;
     var count=0;
@@ -83,7 +101,8 @@ function generateSparkBars(product,data){
         .attr("width", width)
         .attr("height", height)
         .on("click",function(){
-            console.log(product);
+                currentProduct = product;
+                currentCountry = $('#country_select').val();
                 transitionBarChart(filterChartData($('#country_select').val(),product));              
             });
 
@@ -116,11 +135,11 @@ function generateSparkBars(product,data){
                 } else {
                             return "green";
                 }                     
-            })
+            });
 }
 
 function generateBarChart(data){
-    var margin = {top: 0, right: 30, bottom: 30, left: 30},
+    var margin = {top: 10, right: 30, bottom: 30, left: 70},
         width = $("#bar_chart").width() - margin.left - margin.right,
         height =  $("#bar_chart").height() - margin.top - margin.bottom;
     var avg=0;
@@ -136,43 +155,66 @@ function generateBarChart(data){
     
     var datadomain = [];
     data.forEach(function(e){
+        e.key = keyToYear(e.key);
         if(e.value!=0){
             datadomain.push(e.value);
         }
     });
     
     var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .1);
+        .rangeRoundBands([0, width]);
 
     var y = d3.scale.linear()
-        .range([0,height]);    
+        .range([0,height]); 
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    xAxis.tickValues(["Jan 13","Mar 13","May 13","Jul 13","Sep 13","Nov 13","Jan 14","Mar 14","May 14","Jul 14","Sep 14"]);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(5);
     
     x.domain(data.map(function(d) {return d.key; }));
     var ydomain=d3.extent(datadomain,function(d){return d;});
-    y.domain([0,ydomain[1]*1.1]);
+    y.domain([ydomain[1]*1.1,0]);
     
     var svg = d3.select("#bar_chart").append("svg")
-            .attr("width", width)
-            .attr("height", height);
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);    
 
     svg.selectAll("rect")
             .data(data)
             .enter()
             .append("rect")
-            .attr("x", function(d,i) { return x.rangeBand()*i; })
+            .attr("x", function(d,i) { return x(d.key); })
             .attr("width", x.rangeBand()-1)
             .attr("y", function(d){
                         if(d.value==0){
                            return 0;
                         } else {
-                           return height-y(d.value);
+                           return y(d.value);
                         }                
             })
             .attr("height", function(d) {
                         if(d.value==0){
                            return height;
                         } else {
-                            return y(d.value);
+                            return height-y(d.value);
                         }
             })
             .attr("fill",function(d){
@@ -181,19 +223,28 @@ function generateBarChart(data){
                 } else {
                             return "steelblue";
                 }                     
-            });
+            })
+            .on("mouseover",function(d,i){
+                data = filterBarData(i+1);
+                var html = "";
+                data.forEach(function(e){
+                   html = html + "<p>"+ e.adm1_name+": "+e.mp_price+"</p>" ;
+                });
+                $('#marketdata').html(html);
+            });            
             
     svg.append("line")
         .attr("x1", 0)
-        .attr("y1", height-y(avg))
+        .attr("y1", y(avg))
         .attr("x2", width)
-        .attr("y2", height-y(avg))
+        .attr("y2", y(avg))
         .attr("stroke-width", 2)
-        .attr("stroke", "red");
+        .attr("stroke", "red")
+        .attr("id","avgline");
 }
 
 function transitionBarChart(data){
-    var margin = {top: 0, right: 30, bottom: 30, left: 30},
+    var margin = {top: 10, right: 30, bottom: 30, left: 70},
         width = $("#bar_chart").width() - margin.left - margin.right,
         height =  $("#bar_chart").height() - margin.top - margin.bottom;
 
@@ -217,27 +268,39 @@ function transitionBarChart(data){
     avg=total/count;    
     
     var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .1);
+        .rangeRoundBands([0, width]);
 
     var y = d3.scale.linear()
-        .range([0,height]);    
+        .range([0,height]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(5);
     
     x.domain(data.map(function(d) {return d.key; }));
     var ydomain=d3.extent(datadomain,function(d){return d;});
-    y.domain([0,ydomain[1]*1.1]);
+    y.domain([ydomain[1]*1.1,0]);
     
     var svg = d3.select("#bar_chart");
+    
+    svg.select(".y").transition()
+        .call(yAxis); 
 
     svg.selectAll("rect")
             .data(data)
             .transition()
-            .attr("x", function(d,i) { return x.rangeBand()*i; })
+            .attr("x", function(d,i) { return x(d.key); })
             .attr("width", x.rangeBand()-1)
             .attr("y", function(d){
                         if(d.value==0){
                            return 0;
                         } else {
-                           return height-y(d.value);
+                           return y(d.value);
                         }
                 
             })
@@ -245,7 +308,7 @@ function transitionBarChart(data){
                         if(d.value==0){
                            return height;
                         } else {
-                            return y(d.value);
+                            return height-y(d.value);
                         }
             })
             .attr("fill",function(d){
@@ -256,14 +319,88 @@ function transitionBarChart(data){
                 }                     
             });
 
-    svg.select("line")
+    svg.select("#avgline")
         .transition()
         .attr("x1", 0)
-        .attr("y1", height-y(avg))
+        .attr("y1", y(avg))
         .attr("x2", width)
-        .attr("y2", height-y(avg))
+        .attr("y2", y(avg))
         .attr("stroke-width", 2)
         .attr("stroke", "red");    
+}
+
+function keyToYear(key){
+    key=key-1;
+    months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    years = ["13","14"];
+    return months[key%12]+" "+years[parseInt(key/12)];
+}
+
+function generateMap(){
+    var margin = {top: 10, right: 10, bottom: 10, left: 10},
+    width = $('#map').width() - margin.left - margin.right,
+    height = 425;
+   
+    var projection = d3.geo.mercator()
+        .center([8,11])
+        .scale(1200);
+
+    var svg = d3.select('#map').append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    var g = svg.append("g");    
+
+    g.selectAll("path")
+        .data(regions.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("stroke",'#cccccc')
+        .attr("fill",'#ffffff')
+        .attr("opacity",1)
+        .attr("id",function(d){
+            return d.properties.PCODE_REF;
+        })
+        .attr("class","region");
+  
+    
+    var g = svg.append("g"); 
+}
+
+function transitionCountryMap(country){
+    var mapsettings = {"Guinea":{"scale":3200,"centre":[-6.5,8.3]},
+            "Liberia":{"scale":3200,"centre":[-6,5]},
+            "Mali":{"scale":1500,"centre":[5,9]},
+            "Sierra Leone":{"scale":6000,"centre":[-9,7.7]}
+        };
+    
+    var margin = {top: 10, right: 10, bottom: 10, left: 10},
+        width = $('#map').width() - margin.left - margin.right,
+        height = 425;
+
+        var projection = d3.geo.mercator()
+            .center(mapsettings[country].centre)
+            .scale(mapsettings[country].scale);
+    
+    var path = d3.geo.path()
+        .projection(projection);
+    d3.select("#map").selectAll("path")
+        .attr("d", path)
+        .attr("stroke",'#cccccc')
+        .attr("fill",'#ffffff')
+        .attr("opacity",function(d){
+            if(d.properties.CNTRY_NAME==country){
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+        .attr("class","region")
+        .attr("stroke","#000000");
 }
 
 products.forEach(function(e){
@@ -274,5 +411,10 @@ $('#country_select').change(function(){
     onCountrySelect($(this).val());
 });
 
-onCountrySelect("Guinea");
+
+currentCountry = "Guinea";
+currentProduct = "Beans";
 generateBarChart(filterChartData("Guinea","Beans"));
+generateMap();
+
+onCountrySelect("Guinea");
